3e0a28bc91be2056478d001f0fc26cc7830b9910
Alan Omar Solano Campos <alan.solano@citi.com.mx>
2020-06-27 12:04:42 -0500

    Initial Commit

---
+++ LiverpoolTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiverpoolTest
+//
+//  Created by Alan Omar Solano Campos on 27/06/20.
+//  Copyright © 2020 Alan Omar Solano Campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
