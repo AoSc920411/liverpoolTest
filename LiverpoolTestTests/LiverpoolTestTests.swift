59aab9edac7d84fb4ba4b410c4e40e26c87ed825
Alan Omar <aosolano@grupoasesores.com.mx>
2020-09-28 16:50:14 -0500

    include test

---
+++ LiverpoolTestTests/LiverpoolTestTests.swift
@@ -0,0 +1,33 @@
+//
+//  LiverpoolTestTests.swift
+//  LiverpoolTestTests
+//
+//  Created by Alan Omar on 28/09/20.
+//  Copyright © 2020 Alan Omar Solano Campos. All rights reserved.
+//
+
+import XCTest
+
+class LiverpoolTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
